1dbca680080fcc9c7a7decc60674aaa67262b7d2
scs0209 <tjdckdtn2463@naver.com>
2024-06-05 14:28:54 +0900

    style: 기본 채팅 스타일
    - 채팅 인터페이스 관리를 위한 stateful 위젯 `MyHomePage` 구현
    - `ListView.builder`를 사용하여 메시지 목록 표시
    - 사용자 메시지와 봇 메시지를 서로 다른 스타일로 구분
    - 사용자 메시지는 오른쪽에, 봇 메시지는 왼쪽에 정렬
    - 메시지 컨테이너에 적절한 패딩과 테두리 반경 적용

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,17 +1,67 @@
+import 'package:chat_genius/message.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MHMyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MHMyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Message> _messages = [
+    Message(text: "Hi", isUser: true),
+    Message(text: "Hello, what's up", isUser: false),
+    Message(text: "Great and you?", isUser: true),
+    Message(text: "I'm excellent ", isUser: false),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("안녕하세요!")),
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                final message = _messages[index];
+                return ListTile(
+                  title: Align(
+                    alignment: message.isUser
+                        ? Alignment.centerRight
+                        : Alignment.centerLeft,
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: message.isUser ? Colors.blue : Colors.grey[300],
+                        borderRadius: message.isUser
+                            ? BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                bottomRight: Radius.circular(20),
+                                bottomLeft: Radius.circular(20),
+                              )
+                            : BorderRadius.only(
+                                topLeft: Radius.circular(20),
+                                bottomRight: Radius.circular(20),
+                                bottomLeft: Radius.circular(20),
+                              ),
+                      ),
+                      child: Text(
+                        message.text,
+                        style: TextStyle(
+                            color:
+                                message.isUser ? Colors.white : Colors.black),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          // user input
+        ],
+      ),
     );
   }
 }
